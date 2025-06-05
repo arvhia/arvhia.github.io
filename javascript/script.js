9978befc75be9e3783047ffdc23e3a478ec45820
Jose Hernandez <josehdzibarra@hotmail.com>
2025-06-04 23:03:03 -0600

    Se agregó a inicio nuevos slides y footer

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,6 @@ for (let i = 0; i < document.querySelectorAll("#consignar").length; i++) {
     document.querySelectorAll("#consignar")[i].action = `https://wa.me/${numeroCelular}`;
 
 }
+document.querySelector("#alinkNumero").href = `https://wa.me/${numeroCelular}`;
+document.querySelector("#alinkNumero").innerHTML = `${numeroCelular}`;
+
